601d5fc1d67579a6bde65c82a74806cb80684f08
Marc Villacorta <marc.villacorta@gmail.com>
2011-06-10 19:28:18 +0200

    Samba module empty structure.

---
+++ modules/samba/manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: samba
+#
+# This module manages samba
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class samba {
+
+
+}
