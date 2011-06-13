5d704235af22b4f9e451e224584503f7672df3eb
Marc Villacorta <marc.villacorta@gmail.com>
2011-06-13 21:51:17 +0200

    Simplified concat module.

---
+++ modules/concat/manifests/fragment.pp
@@ -0,0 +1,66 @@
+#------------------------------------------------------------------------------ 
+# Define: concat:fragment
+#
+#   This define is part of the concat module.
+#   Based on R.I. Pienaar concat module.
+#
+#   Marc Villacorta <marc.villacorta@gmail.com>
+#   2011-06-13
+#
+#   Tested platforms:
+#       - CentOS 5.6
+#
+# Parameters:
+#
+#   target:    The file that these fragments belong to.
+#   content:   If present puts the content into the file.
+#   source:    If content was not specified, use the source.
+#   order:     By default all files gets a 10_ prefix in the directory you
+#              can set it to anything else using this to influence the order
+#              of the content in the file.
+#   ensure:    Present/Absent or destination to a file.
+#
+# Actions:
+#
+#   Blah, blah ...
+#
+# Sample Usage:
+#
+#   concat::fragment { 'motd_header':
+#       target  => '/etc/motd',
+#       content => template("${module_name}/motd_header.erb"),
+#       order   => '01',
+#   }
+#
+#   or
+#
+#------------------------------------------------------------------------------
+define concat::fragment(
+    $target,
+    $content    = '',
+    $source     = '',
+    $order      = '10',
+    $ensure     = 'present'
+) {
+
+    # Set variables:
+    $safe_target = regsubst($target, '/', '_', 'G')
+    $fragdir = "/var/lib/puppet/concat/${safe_target}"
+
+    # If content is passed, use that, else if source is passed use that.
+    # If neither passed, but $ensure is in symlink form, make a symlink.
+    if $content { File { content => $content } }
+    elsif $source { File { source => $source } }
+    elsif $ensure in [ '', 'absent', 'present', 'file', 'directory' ] {
+        fail('No content, source or symlink specified.')
+    }
+
+    # Place the fragment file:
+    file { "${fragdir}/fragments/${order}_${name}":
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        ensure => $ensure,
+        notify => Exec[ "concat_${target}" ],
+    }
+}
---
+++ modules/concat/manifests/init.pp
@@ -0,0 +1,85 @@
+#------------------------------------------------------------------------------
+# Define: concat
+#
+#   This module provides a simplified file fragments concatenation method.
+#   Based on R.I. Pienaar concat module.
+#
+#   Marc Villacorta <marc.villacorta@gmail.com>
+#   2011-06-13
+#
+#   Tested platforms:
+#       - CentOS 5.6
+#
+# Parameters:
+#
+#   owner: 
+#   group: 
+#   mode: 
+#
+# Sample Usage:
+#
+#   concat { '/etc/motd': }
+#
+#   or
+#
+#   concat { '/etc/motd':
+#       owner => 'root',
+#       group => 'root',
+#       mode  => '0644',
+#   }
+#------------------------------------------------------------------------------
+define concat (
+    $owner  = 'root',
+    $group  = 'root',
+    $mode   = '0644'
+) {
+
+    # Set variables:
+    $safe_name  = regsubst($name, '/', '_', 'G')
+    $fragdir    = "/var/lib/puppet/concat/${safe_name}"
+
+    # Files and directories:
+    file {
+
+         '/var/lib/puppet/concat':
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755';
+
+         "${fragdir}":
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755';
+
+         "${fragdir}/fragments":
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0755';
+
+         "${fragdir}/fragments.concat":
+            ensure  => present,
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644';
+
+         "${name}":
+            ensure  => present,
+            owner   => $owner,
+            group   => $group,
+            mode    => $mode,
+            source  => "${fragdir}/fragments.concat";
+    }
+
+    # Concatenate on refresh only:
+    exec { "concat_${name}":
+        user        => 'root',
+        group       => 'root',
+        refreshonly => true,
+        subscribe   => File[ "${fragdir}/fragments" ],
+        notify      => File[ "${name}" ],
+        command     => "/bin/cat ${fragdir}/fragments/* > ${fragdir}/fragments.concat"
+    }
+}
