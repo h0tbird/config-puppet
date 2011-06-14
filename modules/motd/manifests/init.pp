9664624aa948d89c0c3b97a68c962c6282b4b9c6
Marc Villacorta <marc.villacorta@gmail.com>
2011-06-14 23:25:29 +0200

    Motd module.

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,1 +1,42 @@
-import "*"
+#------------------------------------------------------------------------------
+# Class: motd
+#
+#   This module manages the /etc/motd file.
+#
+#   Marc Villacorta <marc.villacorta@gmail.com>
+#   2011-06-14
+#
+#   Tested platforms:
+#       - CentOS 5.6
+#
+# Actions:
+#
+#   Assemble header and footer fragments from templates.
+#
+# Sample Usage:
+#
+#   include motd
+#
+#   or
+#
+#   class { 'motd': }
+#------------------------------------------------------------------------------
+class motd {
+
+    # Define the target file:
+    concat { '/etc/motd': }
+
+    # Set the file header:
+    concat::fragment { 'motd_header':
+        target  => '/etc/motd',
+        content => template("${module_name}/motd_header.erb"),
+        order   => 01,
+    }
+
+    # Set the file footer:
+    concat::fragment { 'motd_footer':
+        target  => '/etc/motd',
+        content => template("${module_name}/motd_footer.erb"),
+        order   => 99,
+    }
+}
