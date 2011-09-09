4130d63d1c66505091d6851063ce6b6ba630287c
Marc Villacorta <marc.villacorta@gmail.com>
2011-09-09 18:54:51 +0200

    Same thing less code.

--- modules/samba/manifests/user.pp
+++ modules/samba/manifests/user.pp
@@ -15,7 +15,7 @@ define samba::user ( $pass = 'secret' ) {
         user    => 'root',
         group   => 'root',
         path    => ['/bin','/usr/bin'],
-        unless  => "echo -n \"${pass}\" | iconv -f ASCII -t UTF-16LE | openssl dgst -md4 | awk '{print \"pdbedit -wL ${name} | grep -qi \"\$2}' | sh",
+        unless  => "printf ${pass} | iconv -f ASCII -t UTF-16LE | openssl md4 | awk '{print \"pdbedit -wL ${name} | grep -qi \"\$2}' | sh",
         command => "echo -ne \"${pass}\\n${pass}\\n\" | pdbedit -ta ${name}",
     }
 }
