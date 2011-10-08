943c9c419ea557b84a00d3321c645dcf4fc5ccae
Marc Villacorta <marc.villacorta@gmail.com>
2011-10-08 14:05:43 +0200

    Moved puppet_confdir to extdata.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -21,7 +21,7 @@ node 'puppet.popapp.com' {
         # Samba service:
         samba_workgroup   => extlookup('samba_workgroup'),
         samba_hosts_allow => extlookup('samba_hosts_allow'),
-        samba_share_path  => '/etc/puppet',
+        samba_share_path  => extlookup('puppet_confdir'),
         samba_valid_users => [ 'marc', 'debo' ],
     }
 }
