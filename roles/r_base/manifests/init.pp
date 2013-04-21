0d6b4e5846b9a2ad163cf939de52754ad5667782
Marc Villacorta <marc.villacorta@gmail.com>
2013-04-21 02:36:02 +0200

    This makes more sense.

--- roles/r_base/manifests/init.pp
+++ roles/r_base/manifests/init.pp
@@ -12,26 +12,30 @@ class r_base (
     # Stage: pre
     #------------
 
-    class { 'hosts':
-        hosts => $hosts['hosts'],
-        stage => pre,
-    } ->
+    if $hosts {
+        class { 'hosts':
+            hosts => $hosts['hosts'],
+            stage => pre,
+        }
+    }
 
-    class { 'yum':
-        version           => $yum['version'],
-        cachedir          => $yum['cachedir'],
-        keepcache         => $yum['keepcache'],
-        debuglevel        => $yum['debuglevel'],
-        logfile           => $yum['logfile'],
-        exactarch         => $yum['exactarch'],
-        obsoletes         => $yum['obsoletes'],
-        gpgcheck          => $yum['gpgcheck'],
-        plugins           => $yum['plugins'],
-        installonly_limit => $yum['installonly_limit'],
-        bugtracker_url    => $yum['bugtracker_url'],
-        distroverpkg      => $yum['distroverpkg'],
-        repos             => $yum['repos'],
-        stage             => pre,
+    if $yum {
+        class { 'yum':
+            version           => $yum['version'],
+            cachedir          => $yum['cachedir'],
+            keepcache         => $yum['keepcache'],
+            debuglevel        => $yum['debuglevel'],
+            logfile           => $yum['logfile'],
+            exactarch         => $yum['exactarch'],
+            obsoletes         => $yum['obsoletes'],
+            gpgcheck          => $yum['gpgcheck'],
+            plugins           => $yum['plugins'],
+            installonly_limit => $yum['installonly_limit'],
+            bugtracker_url    => $yum['bugtracker_url'],
+            distroverpkg      => $yum['distroverpkg'],
+            repos             => $yum['repos'],
+            stage             => pre,
+        }
     }
 
     #-------------
@@ -40,21 +44,27 @@ class r_base (
 
     include motd
 
-    class { 'ntp':
-        ensure  => $ntp['ensure'],
-        version => $ntp['version'],
-        servers => $ntp['servers'],
-        tickers => $ntp['tickers'],
-    } ->
+    if $ntp {
+        class { 'ntp':
+            ensure  => $ntp['ensure'],
+            version => $ntp['version'],
+            servers => $ntp['servers'],
+            tickers => $ntp['tickers'],
+        }
+    }
 
-    class { 'ssh':
-        ensure    => $ssh['ensure'],
-        version   => $ssh['version'],
-        root_keys => $ssh['root_keys'],
-    } ->
+    if $ssh {
+        class { 'ssh':
+            ensure    => $ssh['ensure'],
+            version   => $ssh['version'],
+            root_keys => $ssh['root_keys'],
+        }
+    }
 
-    class { 'puppet::client':
-        ensure  => $puppet['ensure'],
-        version => $puppet['version'],
+    if $puppet {
+        class { 'puppet::client':
+            ensure  => $puppet['ensure'],
+            version => $puppet['version'],
+        }
     }
 }
